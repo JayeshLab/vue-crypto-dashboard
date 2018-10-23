036d78e51ac12923ade6934bc691d7917030e4a4
JayeshLab <helloiamjayesh@gmail.com>
2018-10-23 14:24:50 +0530

    updated base path

---
+++ docs/precache-manifest.a55184cf21489f52173a1cbd6a5f476d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "a0be713869c6203f3624",
+    "url": "js/chunk-vendors.95fa0417.js"
+  },
+  {
+    "revision": "58688998882feb149691",
+    "url": "js/chunk-bae2.47a42afb.js"
+  },
+  {
+    "revision": "abad62902ca7efba0780",
+    "url": "js/chunk-6f3e.18b937d1.js"
+  },
+  {
+    "revision": "3edcebe059efe100fbda",
+    "url": "js/app.4a7b7037.js"
+  },
+  {
+    "revision": "e5f3d798368a93f38f1ae4bd8e689496",
+    "url": "index.html"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "3edcebe059efe100fbda",
+    "url": "css/app.ed7e315d.css"
+  }
+];
