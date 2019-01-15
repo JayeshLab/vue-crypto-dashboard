30255984ed6870319dea2acaa3d1e75ae579b7aa
myboxapp <jayesh.vachhani@gmail.com>
2019-01-15 19:59:58 +0530

    Build demo

---
+++ docs/precache-manifest.5cc4435a609797a25dccbeced4e57e3a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "a0be713869c6203f3624",
+    "url": "js/chunk-vendors.95fa0417.js"
+  },
+  {
+    "revision": "5fc1f5302fab3e15d2fe",
+    "url": "js/chunk-bae2.eead32c4.js"
+  },
+  {
+    "revision": "e3b612255ab550501205",
+    "url": "js/chunk-6f3e.9bee850e.js"
+  },
+  {
+    "revision": "0bc2c1d6ef16ca1dc550",
+    "url": "js/app.d462a764.js"
+  },
+  {
+    "revision": "136231e4d329698c3fdc0a58e2490a77",
+    "url": "index.html"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "0bc2c1d6ef16ca1dc550",
+    "url": "css/app.75cbbce0.css"
+  }
+];
